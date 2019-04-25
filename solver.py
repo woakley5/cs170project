22e01131483a60d8694b4768cafd20f00d16ff68
Will Oakley <woakley5@icloud.com>
2019-04-25 13:44:44 -0700

    Using some scouts

--- solver.py
+++ solver.py
@@ -4,11 +4,15 @@ import random
 def solve(client):
     client.end()
     client.start()
+    print("Home: " + str(client.home))
 
-    edges = list(nx.dfs_edges(client.G, source = client.home))
+    edges = list(nx.bfs_edges(client.G, source = client.home))
     edges.reverse()
 
     for e in edges:
-        client.remote(e[1], e[0])
-        
+        scoutResult = client.scout(e[1], list(range(1, client.students)))
+        scoutValues = list(scoutResult.values())
+        if scoutValues.count(True) >= len(scoutValues)/2:
+            client.remote(e[1], e[0])
+    print(client.bot_locations) 
     client.end()
